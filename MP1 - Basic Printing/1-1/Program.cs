46db0bdf411719833374620657d5faa2d2a7286f
Syveraia <dannleyesa@gmail.com>
2023-08-30 21:50:29 +0800

    updated files

---
+++ MP1 - Basic Printing/1-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeyesaMP1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Vision
+Adamson University, a Catholic, Vincentian educational institution, is a recognized leading center for quality education particularly for the socially disadvantaged.
+
+Mission
+As a Catholic University, we diligently pursue truth and knowledge, inspired by Gospel values and guided by the teachings of the Church;
+
+As a Vincentian community, we inspire others to follow the example of St. Vincent de Paul , who led and organized his contemporaries in creatively responding to those who are in need;
+
+As an institution of learning, we assist in the formation of competent, creative and socially responsible leaders through our commitment to excellence in discovery, learning and service;
+
+As a catalyst of social transformation, we provide quality services that empower others to become agents of change.");
+            Console.ReadKey();
+        }
+    }
+}
